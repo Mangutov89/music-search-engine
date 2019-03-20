e278a09b119c44232022ebd7ae783b1e91a8df4f
Leo Mangutov and Victoria Martinez <victoria@>
2019-03-20 11:51:39 -0700

    Add music API search

--- src/index.js
+++ src/index.js
@@ -2,3 +2,24 @@ import 'bootstrap';
 import $ from 'jquery';
 import '../node_modules/bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
+import { ArtistSearch } from './artistAPI.js';
+
+$(document).ready(function() {
+  $("#artist-form").submit(function(event) {
+    event.preventDefault();
+    const artistInput = $("#artist").val();
+    $("#artist").val("");
+
+    let newArtist = new ArtistSearch();
+    let promise = newArtist.getArtist(artistInput);
+
+    promise.then(function(response) {
+       let body =JSON.parse(response);
+       console.log(body);
+       $('#results').text(`Your artist is ${artistInput} and his facebook is <a href=${body.artists[0].strFacebook}>click here</a>`);
+    }, function(error) {
+       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+
+    });
+  });
+});
