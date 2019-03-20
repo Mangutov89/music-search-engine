ae2e669bf3a3cd814ddd756f729ea60f32534dd5
Leo Mangutov and Victoria Martinez <victoria@>
2019-03-20 16:21:13 -0700

    add ticket master api

--- src/artistAPI.js
+++ src/artistAPI.js
@@ -15,3 +15,21 @@ export class ArtistSearch {
     });
   }
 }
+
+export class EventSearch {
+  getEvent(blah) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TICKET_MASTER}&keyword=${blah}&stateCode=OR&city=portland&sort=date,asc&size=20`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import $ from 'jquery';
 import '../node_modules/bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
 import { ArtistSearch } from './artistAPI.js';
+import { EventSearch } from './artistAPI.js';
 
 $(document).ready(function() {
+
   $("#artist-form").submit(function(event) {
     event.preventDefault();
     const artistInput = $("#artist").val();
@@ -16,10 +18,36 @@ $(document).ready(function() {
     promise.then(function(response) {
        let body =JSON.parse(response);
        console.log(body);
-       $('#results').text(`Your artist is ${artistInput} and his facebook is <a href=${body.artists[0].strFacebook}>click here</a>`);
+       $('#results').text("your artist you chose is " + artistInput + " their facebook page is " + body.artists[0].strFacebook);
+       $('#results-bio').text(`Learn more about this artist: ${body.artists[0].strBiographyEN}`);
+       setTimeout(function() {
+         $('#results-pic').html(`here is a picture <img src="${body.artists[0].strArtistFanart}">`);
+       }, 3000);
     }, function(error) {
        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
 
+
+
+  });
+  $("#event-form").submit(function(event) {
+    event.preventDefault();
+    const eventInput = $("#event").val();
+
+    let newEvent = new EventSearch();
+    let promise = newEvent.getEvent(eventInput)
+
+
+    promise.then(function(response) {
+      let body =JSON.parse(response);
+      console.log(body._embedded.events[0]);
+      $('#event-results').text(`${eventInput} next event is: ${body._embedded.events[0].name}`);
+      // $('#results-bio').text(`Learn more about this artist: ${body.artists[0].strBiographyEN}`);
+      // setTimeout(function() {
+      //   $('#results-pic').html(`here is a picture <img src="${body.artists[0].strArtistFanart}">`);
+      // }, 3000);
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
 });
